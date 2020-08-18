84cc08a371023030397a31cf36fc898857fab5e8
Rafael Flores <rafac@unitec.edu>
2020-08-17 21:00:54 -0600

    Base de Datos: Estudiante y fechaexam

--- aula-prolog.pl
+++ aula-prolog.pl
@@ -68,31 +68,143 @@ aula(ovm92, 35, 2).
 aula(wia17, 10, 1).
 aula(lkv19, 10, 1).
 
-estudiante(2000100001, 1, [math101, phys201, ec201]) .
-estudiante(2000100002, 1, [math101, phys201, hist301]).
-estudiante(2000100003, 1, [math101, ec201, hist301]) .
-estudiante(2000100004, 1, [math101, phys201, ec201, hist301]) .
-estudiante(2000100007, 0, [ec201, phys201]).
-estudiante(2000100015, 0, [ec201, hist301]) .
-estudiante(2000100016, 0, [cmpe150, hist301]).
-estudiante(2000100017, 0, []).
-estudiante(2000100008, 0, [math101, phys201]) .
-estudiante(2000100009, 1, [phys201, ec201]).
-estudiante(2000100010, 0, [cmpe150, math101]).
-estudiante(2000100011, 0, [cmpe150, hist301]).
-estudiante(2000100012, 1, [math101, phys201]).
-estudiante(2002100009, 0, [math101, ec201, hist301]).
-estudiante(2002100071, 0, [phys201, ec201, hist301]).
-estudiante(2002100053, 0, [cmpe150, ec201]).
-estudiante(2002100040, 1, [math101, hist301]).
-estudiante(2002100032, 0, [phys201, ec201]).
-estudiante(2002100010, 0, [cmpe150]).
-estudiante(2002103010, 1, []).
-fechaexam(cmpe150, ['15.04.2018', '25.05.2018', '04.06.2018']).
-fechaexam(math101, ['17.04.2018', '15.05.2018', '04.06.2018']).
-fechaexam(phys201, ['15.04.2018', '19.05.2018', '05.06.2018']).
-fechaexam(ec201, ['16.04.2018', '20.05.2018', '02.06.2018']).
-fechaexam(hist301, ['14.04.2018', '21.05.2018', '01.06.2018']).
+estudiante(3001, 1, [fis201, tll314]).
+estudiante(3002, 1, [eie1]).
+estudiante(3003, 1, [esp103]).
+estudiante(3004, 1, [soc101, lcc104, mat101, ccc107]).
+estudiante(3005, 0, [lcc104, esp103]).
+estudiante(3006, 1, [soc101, esp103, eie1]).
+estudiante(3007, 0, [fis201, soc101, tll314]).
+estudiante(3008, 1, []).
+estudiante(3009, 0, [sel406]).
+estudiante(3010, 1, [adm102, soc101, eie1]).
+estudiante(3011, 0, [lcc104]).
+estudiante(3012, 1, [esp103]).
+estudiante(3013, 1, []).
+estudiante(3014, 1, [mat101, tll314, eie1]).
+estudiante(3015, 0, []).
+estudiante(3016, 1, [fis201, adm102, tll314, lcc104]).
+estudiante(3017, 1, [ccc107, tll314]).
+estudiante(3018, 1, [lcc104, mat101, fis201]).
+estudiante(3019, 1, []).
+estudiante(3020, 0, [sel406, mat101, his101, tll314]).
+estudiante(3021, 0, [sel406, lcc104, eie1, soc101]).
+estudiante(3022, 1, [fis201, lcc104, sel406, esp103]).
+estudiante(3023, 1, [his101, tel201, adm102, ccc107]).
+estudiante(3024, 0, [adm102, esp103, lcc104]).
+estudiante(3025, 0, []).
+estudiante(3026, 1, []).
+estudiante(3027, 0, [sel406, adm102]).
+estudiante(3028, 0, []).
+estudiante(3029, 0, [eie1, ccc107, tel201, soc101]).
+estudiante(3030, 1, [sel406, fis201, his101, tel201]).
+estudiante(3031, 1, []).
+estudiante(3032, 1, [ccc107, sel406]).
+estudiante(3033, 0, [tll314, esp103, fis201]).
+estudiante(3034, 0, [adm102]).
+estudiante(3035, 1, [tll314, ccc107, his101, esp103]).
+estudiante(3036, 1, []).
+estudiante(3037, 0, [fis201]).
+estudiante(3038, 1, [tll314]).
+estudiante(3039, 1, [sel406, tel201]).
+estudiante(3040, 1, [lcc104, tel201, mat101, tll314]).
+estudiante(3041, 0, []).
+estudiante(3042, 0, [eie1, ccc107]).
+estudiante(3043, 1, [adm102]).
+estudiante(3044, 0, [ccc107, sel406]).
+estudiante(3045, 0, [sel406]).
+estudiante(3046, 0, [lcc104, tel201]).
+estudiante(3047, 0, [his101, tel201, mat101, soc101]).
+estudiante(3048, 0, [fis201, esp103, eie1]).
+estudiante(3049, 1, []).
+estudiante(3050, 1, [fis201]).
+estudiante(3051, 1, [his101, lcc104]).
+estudiante(3052, 0, [tel201, tll314, his101, adm102]).
+estudiante(3053, 1, [mat101, lcc104]).
+estudiante(3054, 1, [mat101, adm102]).
+estudiante(3055, 1, [tll314, soc101, tel201]).
+estudiante(3056, 1, [soc101, ccc107, fis201]).
+estudiante(3057, 1, []).
+estudiante(3058, 1, [esp103, tll314, fis201, adm102]).
+estudiante(3059, 1, [adm102, tel201, tll314, eie1]).
+estudiante(3060, 1, [sel406]).
+estudiante(3061, 1, [esp103, fis201, his101]).
+estudiante(3062, 1, [esp103, fis201]).
+estudiante(3063, 0, [ccc107, lcc104, soc101]).
+estudiante(3064, 1, []).
+estudiante(3065, 0, []).
+estudiante(3066, 1, [fis201, soc101, his101, tll314]).
+estudiante(3067, 1, [his101]).
+estudiante(3068, 1, []).
+estudiante(3069, 1, [his101, sel406]).
+estudiante(3070, 1, [fis201, esp103, eie1, mat101]).
+estudiante(3071, 1, []).
+estudiante(3072, 0, [esp103, mat101, lcc104, tll314]).
+estudiante(3073, 1, [esp103]).
+estudiante(3074, 0, [esp103, his101, tel201]).
+estudiante(3075, 1, [soc101, esp103]).
+estudiante(3076, 1, [mat101, tel201]).
+estudiante(3077, 1, [sel406]).
+estudiante(3078, 1, [esp103, fis201]).
+estudiante(3079, 0, [esp103]).
+estudiante(3080, 0, [adm102, fis201, tll314, mat101]).
+estudiante(3081, 0, [soc101]).
+estudiante(3082, 0, [eie1, tel201]).
+estudiante(3083, 1, [adm102, eie1, esp103, fis201]).
+estudiante(3084, 0, [esp103, tll314, ccc107]).
+estudiante(3085, 0, [eie1, his101, adm102, tel201]).
+estudiante(3086, 0, [fis201, adm102]).
+estudiante(3087, 0, [lcc104]).
+estudiante(3088, 0, [fis201]).
+estudiante(3089, 1, []).
+estudiante(3090, 0, [soc101]).
+estudiante(3091, 1, [soc101, sel406, ccc107, fis201]).
+estudiante(3092, 1, [ccc107, tel201]).
+estudiante(3093, 1, []).
+estudiante(3094, 0, [sel406, ccc107, tll314]).
+estudiante(3095, 0, [soc101, tel201]).
+estudiante(3096, 1, [esp103, adm102, tel201, ccc107]).
+estudiante(3097, 0, [mat101, sel406]).
+estudiante(3098, 0, [mat101, soc101]).
+estudiante(3099, 1, [mat101, adm102]).
+estudiante(3100, 0, []).
+estudiante(3101, 1, [fis201]).
+estudiante(3102, 0, []).
+estudiante(3103, 0, []).
+estudiante(3104, 0, [fis201, his101, ccc107]).
+estudiante(3105, 1, [soc101]).
+estudiante(3106, 0, [tel201]).
+estudiante(3107, 1, [adm102, eie1, sel406]).
+estudiante(3108, 0, [mat101, eie1, lcc104, adm102]).
+estudiante(3109, 1, [his101, adm102, fis201]).
+estudiante(3110, 0, [esp103, eie1]).
+estudiante(3111, 1, [ccc107, adm102, mat101, tel201]).
+estudiante(3112, 1, [sel406]).
+estudiante(3113, 1, []).
+estudiante(3114, 0, [soc101]).
+estudiante(3115, 1, [adm102]).
+estudiante(3116, 0, [tll314, adm102]).
+estudiante(3117, 0, [sel406, eie1, lcc104, adm102]).
+estudiante(3118, 0, [mat101]).
+estudiante(3119, 1, [ccc107, mat101, tll314]).
+estudiante(3120, 0, [lcc104, his101, adm102, mat101]).
+estudiante(3121, 0, []).
+estudiante(3122, 0, [lcc104, adm102, tll314, mat101]).
+estudiante(3123, 1, [his101, eie1]).
+estudiante(3124, 0, [adm102, sel406, fis201, esp103]).
+
+fechaexam(mat101, ['05.03.2020', '24.04.2020', '13.06.2020', '02.08.2020']).
+fechaexam(esp103, ['06.03.2020', '25.04.2020', '14.06.2020', '03.08.2020']).
+fechaexam(his101, ['07.03.2020', '26.04.2020', '15.06.2020', '04.08.2020']).
+fechaexam(lcc104, ['08.03.2020', '27.04.2020', '16.06.2020', '05.08.2020']).
+fechaexam(soc101, ['09.03.2020', '28.04.2020', '17.06.2020', '06.08.2020']).
+fechaexam(ccc107, ['10.03.2020', '29.04.2020', '18.06.2020', '07.08.2020']).
+fechaexam(eie1, ['11.03.2020', '30.04.2020', '19.06.2020', '08.08.2020']).
+fechaexam(fis201, ['12.03.2020', '01.05.2020', '20.06.2020', '09.08.2020']).
+fechaexam(sel406, ['13.03.2020', '02.05.2020', '21.06.2020', '10.08.2020']).
+fechaexam(adm102, ['14.03.2020', '03.05.2020', '22.06.2020', '11.08.2020']).
+fechaexam(tll314, ['15.03.2020', '04.05.2020', '23.06.2020', '12.08.2020']).
+fechaexam(tel201, ['16.03.2020', '05.05.2020', '24.06.2020', '13.08.2020']).
 
 equal(X,X).
 buscar(X,[W|Z]):- equal(X,W);
